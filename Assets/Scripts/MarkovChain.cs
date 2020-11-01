7ad442e5401a1252d2c059488788eb4a7d2acfdd
jaden <cep450@nyu.edu>
2020-11-01 04:46:06 -0500

    working out how to represent the chain as a linked list

--- Assets/Scripts/MarkovChain.cs
+++ Assets/Scripts/MarkovChain.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MarkovChain : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,99 @@ public class MarkovChain : MonoBehaviour
     {
         
     }
+
+
+}
+
+
+/*
+object/class? struct? enum?
+*/
+
+    //Link emptySpace, buildingFront, buildingMid, building
+
+
+public class Chain {
+
+    EmptySpace emptySpace;
+    BuildingFront buildingFront;
+    BuildingMid buildingMid;
+    BuildingEnd buildingEnd;
+
+    public void initChain() {
+
+        emptySpace = new EmptySpace(
+            arr(emptySpace, buildingFront),
+            arr(0.7, 0.3)
+        );
+
+        buildingFront = new BuildingFront(
+            arr(buildingMid),
+            arr(1)
+        );
+
+        buildingMid = new BuildingMid(
+            arr(buildingMid, buildingEnd),
+            arr(0.7, 0.3)
+        );
+
+        buildingEnd = new BuildingEnd(
+            arr(emptySpace),
+            arr(1)
+        );
+    }
+
+
+    ChainLink [] arr(ChainLink, ...) {
+        //return an array
+    }
+    float [] arr(float, ...) {
+        //return an array 
+    }
+}
+
+public class ChainLink {
+
+    ChainLink [] links;
+    float [] chances;
+
+    public ChainLink(ChainLink [] links, float [] ch) {
+
+    }
+    public void generate();
+
+
+}
+
+
+public class EmptySpace : ChainLink {
+
+    public void generate() {
+        print(" ");
+    }
+    
+}
+
+public class BuildingFront : ChainLink {
+
+    public void generate() {
+        print("[");
+    }
+    
+}
+
+public class BuildingMid : ChainLink {
+
+    public void generate() {
+        print("-");
+    }
+    
+}
+
+public class BuildingBack : ChainLink {
+
+    public void generate() {
+        print("]");
+    }
+    
 }
