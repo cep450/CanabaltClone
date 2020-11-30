adddde98d8ea3e1a8248d9aa53deeb8dd275c3ee
Steven <yh3133@nyu.edu>
2020-11-30 17:54:39 +0800

    added hotkey "R" for restart

--- Assets/Scripts/Player/CameraControls.cs
+++ Assets/Scripts/Player/CameraControls.cs
@@ -9,12 +9,9 @@ public class CameraControls : MonoBehaviour
     private Vector3 target; // camera target position
     private Vector3 velocity = Vector3.zero; // camera damp v
 
+    public float gate; // start of the first building after intro
+
     public PlayerRunning playerRunning;
-    
-    void Start()
-    {
-        
-    }
 
     void FixedUpdate()
     {
@@ -22,14 +19,13 @@ public class CameraControls : MonoBehaviour
         playerPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
 
         if (playerRunning.death != true) { // only follows when player isn't dead
-            if (player.position.y >= -5f) { // starting camera position
+            if (player.position.x < gate) { // start in-game follow after exiting first building
                 target = new Vector3(10f, -4f, 0f);
-                transform.position = playerPosition + target;
             }
             else { // camera smooth follow
                 target = new Vector3(10f, 0f, 0f);
-                transform.position = Vector3.SmoothDamp(transform.position, target + playerPosition, ref velocity, 0.15f);
             }
+            transform.position = Vector3.SmoothDamp(transform.position, target + playerPosition, ref velocity, 0.15f);
         }
     }
 }
