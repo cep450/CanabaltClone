849b8e6258700f54f372a7afa6a6afc53926b059
jaden <cep450@nyu.edu>
2020-12-03 19:44:02 -0500

    makeshift building generation! needs tuning tho.

--- Assets/Scripts/Generation/BuildingCreator.cs
+++ Assets/Scripts/Generation/BuildingCreator.cs
@@ -17,38 +17,59 @@ public class BuildingCreator : MonoBehaviour
 
     public void generate(float height, float length, float xpos) {
 
+
         //set the length- widen the base prefab 
-        Vector3 lengthVec3 = new Vector3(length * 1f, 1f, 1f);
+        Vector3 lengthVec3 = new Vector3(length, 1f, 1f);
         transform.localScale += lengthVec3;
-
-        Debug.Log("length " + length + ", transform.localscale.x=" + transform.localScale.x);
-
-        //but, make sure the children aren't scaled horizontally. (undo their scaling)
-        float lengthUndo = 1f/length;
-        Transform [] childTransforms = GetComponentsInChildren<Transform>();
-        foreach(Transform t in childTransforms) {
-            t.localScale = new Vector3(t.localScale.x * lengthUndo, t.localScale.y, t.localScale.z);
-        }
 
 
         //set the x position. but don't forget to account for the length
         //(ie actual pos is minus half the length)
         float xToTranslate = xpos - (length / 2f);
 
+        //and, the y to move- these are usually set y = -something to make their tops at 0, so 
+        //WAIT NVM this is using transform.translate.
+        //float yToTranslate = transform.position.y + height;
+
         //now, set that and set the height- move the building up from the baseline y=0
         transform.Translate(xToTranslate, height, 0f);
 
-        //but, if there's a killplane, move it back down to 0 
-        foreach(Transform t in childTransforms) {
-            if(t.gameObject.name.Equals("Killplane")) { //has to be name cause tag is used- deathTrigger
-                t.Translate(0, -height, 0);
-            }
-        }
+        //but, make sure the children aren't scaled horizontally. (undo their scaling)
+        float lengthUndo = 1f/length;
 
+        Transform [] childTransforms = GetComponentsInChildren<Transform>();
+
+        //fix the children 
+        foreach(Transform t in childTransforms) {
+
+            if(t.gameObject.name.Equals("Killplane")) { //has to be name cause tag is used- deathTrigger
+                //move killplanes down to y=0
+                t.Translate(0, -height, 0);
+            } else if(!t.Equals(transform) && !t.gameObject.tag.Equals("dontSquishMe")) {
+                //if it's not a killplane, the original, or something not to change, fix the width
+                t.localScale = new Vector3(t.localScale.x * lengthUndo, t.localScale.y, t.localScale.z);
+            }
+            
+        }
 
         //finally, decorate it with sprites :]
         //tile sprites, and 
         //add a thing on top 
+
+        //TODO
+        tileSprites();
+        putTopOfBuildingSprite();
+
+    }
+
+
+    void tileSprites() {
+
+        //TODO
+
+    }
+
+    void putTopOfBuildingSprite() {
 
         //TODO
 
